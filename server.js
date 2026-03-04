f2f1af81c1c862a360fe486300c4f803a47bb4fe
Abhinaya <abhinayaswarna@gmail.com>
2026-03-04 12:20:18 +0530

    backend ready for deployment

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const pharmacyRoutes = require('./routes/pharmacy');
 const publicRoutes = require('./routes/public');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
